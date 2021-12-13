1d3b8e6758ac99010398e6fda36df091d4e13b5a
finalrepublic <finalrepublic@outlook.com>
2021-12-13 17:59:56 +0800

    improve CE

--- criterion.py
+++ criterion.py
@@ -3,4 +3,11 @@ from torch.nn.functional import softmax
 
 def CrossEntropyLoss(prediction, label):
     softmax = torch.nn.Softmax(dim=1)
-    return torch.log(softmax(prediction)[0][label])*torch.tensor(-1)
+    # return torch.log(softmax(prediction)[0][label])*torch.tensor(-1)
+    return torch.sum(torch.mul(torch.log(softmax(prediction).view(-1)[label]), torch.tensor(-1)))
+
+def KappaLoss(prediction, label):
+    pass
+
+def QWK(matrix):
+    pass
